93498b8688ee6adbd1fcf800568bb08f2d37b137
Joseph McCann <joe.mccann@mailbox.org>
2025-02-02 01:31:53 -0500

    Fix bug in response curve when GUI loads.

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -79,6 +79,7 @@ private:
     SimpleEQAudioProcessor& audioProcessor;
     juce::Atomic<bool> parametersChanged{ false };
     MonoChain monoChain;
+    void updateChain();
 };
 
 class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor
